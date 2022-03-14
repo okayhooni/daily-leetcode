580f4371947882b7de79951c8e4af9839426fb33
okayhooni <ok9897@gmail.com>
2022-03-14 23:08:01 +0900

    simplify-path

---
+++ 202203/20220314-simplify-path.py
@@ -0,0 +1,27 @@
+"""
+https://leetcode.com/problems/simplify-path/submissions/
+"""
+
+
+class Solution:
+    """
+    시간 복잡도: O(n)
+    공간 복잡도: O(n)
+    """
+    def simplifyPath(self, path: str) -> str:
+        path_elements = path.split('/')
+        path_stack = []
+
+        for e in path_elements:
+            if not e or e == '.':
+                continue
+
+            if e == '..':
+                try:
+                    path_stack.pop()
+                except IndexError:
+                    pass
+            else:
+                path_stack.append(e)
+
+        return '/' + '/'.join(path_stack)
