94bdff0c31d3157d769f055643e8bce24837ade6
okayhooni <ok9897@gmail.com>
2022-03-21 19:32:49 +0900

    add 24ms sample submission with str.rfind()

--- 202203/20220321-partition-labels.py
+++ 202203/20220321-partition-labels.py
@@ -6,6 +6,12 @@ from collections import defaultdict
 
 
 class Solution:
+    """
+    모든 풀이 공통
+
+    시간 복잡도: O(n)
+    공간 복잡도: O(n)
+    """
     def partitionLabels(self, s: str) -> List[int]:
 
         idx_list_by_char = defaultdict(list)
@@ -27,6 +33,25 @@ class Solution:
             idx += 1
 
         return [last_idx - start_idx + 1 for start_idx, last_idx in chunks]
+
+    def partitionLabels_24ms_sample(self, s: str) -> List[int]:
+        """
+        python str 내장 메서드 중에 rfind() & rindex() 가 있구나..
+        """
+
+        flag, start = -1, 0
+        ans, seen = [], set()
+
+        for idx, char in enumerate(s):
+            if char not in seen:
+                seen.add(char)
+                last = s.rfind(char)
+                if last > flag:
+                    flag = last
+            if flag == idx:
+                ans.append(flag - start + 1)
+                start = flag + 1
+        return ans
 
 
 if __name__ == '__main__':
