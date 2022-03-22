e0eb3e466cc0f13f3e36830ebdfc7b8619d4c693
okayhooni <ok9897@gmail.com>
2022-03-23 01:51:13 +0900

    smallest-string-with-a-given-numeric-value

---
+++ 202203/20220322-smallest-string-with-a-given-numeric-value.py
@@ -0,0 +1,33 @@
+class Solution:
+    def getSmallestString(self, n: int, k: int) -> str:
+        """
+        시간 복잡도: O(n)
+        공간 복잡도: O(n)
+        """
+        res = ''
+        residual = k
+
+        for i in range(n):
+            cur_opti_min = max(residual - 26 * (n - 1 - i), 1)
+
+            res += chr(ord('a') + cur_opti_min - 1)
+            residual -= cur_opti_min
+
+        return res
+
+    def getSmallestString_24ms_sample(self, n: int, k: int) -> str:
+        """
+        시간 복잡도: O(1)
+        공간 복잡도: O(n)
+        """
+        diff = k - n
+        quotient = diff // 25
+        reminder = diff % 25
+        ans = "a" * (n-quotient-1) + chr(97+reminder) + "z" * quotient if reminder \
+            else "a" * (n-quotient) + "z" * quotient
+        return ans
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.getSmallestString(92170, 99288))
