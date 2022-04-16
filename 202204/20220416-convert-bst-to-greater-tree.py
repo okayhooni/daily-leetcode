eabe72bb0eea9f86d7ac99fec8c0853e5256b985
okayhooni <ok9897@gmail.com>
2022-04-16 14:04:25 +0900

    convert-bst-to-greater-tree

---
+++ 202204/20220416-convert-bst-to-greater-tree.py
@@ -0,0 +1,38 @@
+"""
+https://leetcode.com/problems/convert-bst-to-greater-tree/
+"""
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    """
+    시간 복잡도: O(n)
+    공간 복잡도: O(1)
+    """
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return
+        accumulated = 0
+
+        def _convert_to_gst(node: TreeNode):
+            nonlocal accumulated
+
+            if node.right:
+                _convert_to_gst(node.right)
+
+            node.val += accumulated
+            accumulated = node.val
+
+            if node.left:
+                _convert_to_gst(node.left)
+
+        _convert_to_gst(root)
+        return root
