231090f824a91a9d7fe0de94ac59a3a25fcbe550
okayhooni <ok9897@gmail.com>
2022-05-26 12:35:15 +0900

    easy num of 1 bits

---
+++ 202205/20220526-number-of-1-bits.py
@@ -0,0 +1,24 @@
+"""
+https://leetcode.com/problems/number-of-1-bits/
+"""
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        return bin(n).count('1')
+
+    def hammingWeightBitwise(self, n: int) -> int:
+        count = 0
+
+        while n > 0:
+            # print(n, bin(n))
+            count += (n % 2 != 0)
+            n = n >> 1
+
+        return count
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.hammingWeight(100))
+    print(sol.hammingWeightBitwise(100))
