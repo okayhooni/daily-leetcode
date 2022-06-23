24848fbd3bbb66005f40f640fa1c0487aa55a7c2
okayhooni <ok9897@gmail.com>
2022-06-24 02:21:34 +0900

    good

--- 202206/20220622-kth-largest-element-in-an-array.py
+++ 202206/20220622-kth-largest-element-in-an-array.py
@@ -41,6 +41,16 @@ class Solution:
         return heap[0]
 
 
+'''
+def heappushpop(heap, item):
+    """Fast version of a heappush followed by a heappop."""
+    if heap and heap[0] < item:
+        item, heap[0] = heap[0], item
+        _siftup(heap, 0)
+    return item
+'''
+
+
 def nlargest(n, iterable, key=None):
     """Find the n largest elements in a dataset.
 
